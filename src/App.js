a62d2bf171ed3903798348c88774ec36ab79ae9a
Gamaunov <gamaunov1911@gmail.com>
2023-04-27 18:53:26 +0300

    fixed slider

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="container">
       <Header />
-      {/* <Slider />
-      <Categories /> */}
+      <Slider />
+      {/* <Categories /> */}
     </div>
   );
 }
