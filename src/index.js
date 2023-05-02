e97194a564bd8705146bb70211a284d9528f3ecb
Gamaunov <gamaunov1911@gmail.com>
2023-05-02 19:58:42 +0300

    created redux-persist > cart

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import store, { persistor } from './redux/store';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -20,7 +22,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import bookSlice from './bookSlice/bookSlice';
 import categorySlice from './categorySlice/categorySlice';
 import topBookSlice from './topBookSlice/topBookSlice';
@@ -6,13 +17,32 @@ import startIndexSlice from './startIndex/startIndexSlice';
 import searchValueSlice from './searchValueSlice/searchValueSlice';
 import cartSlice from './cartSlice/cartSlice';
 
-export default configureStore({
-  reducer: {
-    bookApi: bookSlice,
-    category: categorySlice,
-    topBook: topBookSlice,
-    startIndex: startIndexSlice,
-    search: searchValueSlice,
-    cart: cartSlice,
-  },
+const rootReducer = combineReducers({
+  bookApi: bookSlice,
+  category: categorySlice,
+  topBook: topBookSlice,
+  startIndex: startIndexSlice,
+  search: searchValueSlice,
+  cart: cartSlice,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['bookApi', 'category', 'topBook', 'startIndex', 'search'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
